19d3026d4c9e4eda47f770c3618fcc34f50e09ab
Karthik Murthy <karthik.murthy123@gmail.com>
2016-05-12 23:35:38 -0400

    Update MyBroadcastReceiver.java

--- app/src/main/java/com/example/milan/textsaver/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/milan/textsaver/MyBroadcastReceiver.java
@@ -16,17 +16,22 @@ import java.util.Calendar;
 
     public class MyBroadcastReceiver extends BroadcastReceiver {
         public static final String MyPREFERENCES = "MyPrefs11";
-        public static final String PNUM_KEY = "numKey11";
+        public static final String HOUR = "NOTIF_HOUR";
+        public static final String MINUTE = "NOTIF_MINUTE";
+        
         SharedPreferences sharedpreferences;
         public void onReceive(Context context, Intent intent){
             sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedpreferences.edit();
-
+            int hour = 8;
+            int minute = 10;
             Calendar calendar = Calendar.getInstance();
             //Intent service1 = new Intent(context, MyAlarmService.class);
             //context.startService(service1);
             if(calendar.get(Calendar.HOUR_OF_DAY) > 15 || calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY
                     || calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+                        hour = sharedpreferences.putInt(HOUR, 8);
+                        hour = sharedpreferences.putInt(HOUR, 8);
                         
                         //reset to 8:10 and setalarm
                     }
